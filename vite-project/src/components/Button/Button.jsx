d612319a1aa7f4c2678f935af2473b72f7198b7e
Kenan <galandarovkenan057@gmail.com>
2026-02-17 00:15:48 +0500

    HomePage, questionCard

--- vite-project/src/components/Button/Button.jsx
+++ vite-project/src/components/Button/Button.jsx
@@ -1,8 +1,9 @@
 import cls from "./Button.module.css";
 
-export const Button = ({ isDisabled, isActive, children }) => {
+export const Button = ({ isDisabled, isActive, children, ...props }) => {
   return (
     <button
+      {...props}
       className={`${cls.btn} ${isActive ? cls.isActive : ""}`}
       disabled={isDisabled}
     >
